c02575abbd774a6b185535fab35c527bdb8edc3e
Tom Draper <tomjdraper1@gmail.com>
2019-05-22 16:05:44 +0100

    Add colour generator

--- RouletteSimulator/src/NumberGenerator.java
+++ RouletteSimulator/src/NumberGenerator.java
@@ -2,13 +2,21 @@ import java.util.Random;
 
 public class NumberGenerator {
 
-  public int generateNumber() {
+  private Random random = new Random();
 
-    Random random = new Random();
+  public int generateNumber() {
     int randomNumber = Math.abs(random.nextInt()) % 37;
 
     System.out.println(randomNumber);
     
+    return randomNumber;
+  }
+
+  public int generateColour() {
+    int randomNumber = Math.abs(random.nextInt()) % 2;
+
+    System.out.println(randomNumber);
+
     return randomNumber;
   }
 }
