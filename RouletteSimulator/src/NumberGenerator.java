fb5dbafe00153f33edf114edc1456da9f32b7923
Tom Draper <tomjdraper1@gmail.com>
2019-05-22 15:21:36 +0100

    Initialise number generator

---
+++ RouletteSimulator/src/NumberGenerator.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class NumberGenerator {
+
+  private ArrayList<Integer> redList = new ArrayList<>();
+  private ArrayList<Integer> blackList = new ArrayList<>();
+
+  public int generateNumber() {
+    int toReturn = 0;
+
+    
+    return toReturn;
+  }
+}
